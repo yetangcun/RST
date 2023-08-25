f96dde6707d3179c868e97e098b4033d6cf44a4b
yetangcun <302982814@qq.com>
2023-08-25 17:32:19 +0800

    ...

--- BaseDemo/hello_rust/src/main.rs
+++ BaseDemo/hello_rust/src/main.rs
@@ -1,3 +1,15 @@
+use std::io;
+
 fn main() {
-    println!("Hello, rust!");
+    // println!("Hello, rust!");
+    println!("welcome, rust!");
+
+    println!("please input:");
+    let mut guess = String::new();
+
+    io::stdin()
+    .read_line(&mut guess)
+    .expect("failed to read line");
+
+    println!("you guessed: {guess}")
 }
