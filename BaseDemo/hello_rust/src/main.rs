40a9f68e2f28419ec9380965e20111a317009987
yetangcun <302982814@qq.com>
2023-09-27 18:33:35 +0800

    ...

--- BaseDemo/hello_rust/src/main.rs
+++ BaseDemo/hello_rust/src/main.rs
@@ -1,19 +1,37 @@
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
-fn main() {
-    // println!("Hello, rust!");
+fn main() { // println!("Hello, rust!");
     println!("welcome to rust!");
+    
+    let _sec_num = rand::thread_rng().gen_range(100..=1000);
+    println!("the real number is ：{_sec_num}");
 
-    println!("please input:");
+    loop {
+        println!("please input:");
 
-    let mut guess = String::new(); // mut使变量是可变变量
+        let mut guess = String::new(); // mut使变量是可变变量
 
-    io::stdin()
-    .read_line(&mut guess)
-    .expect("failed to read line");
+        io::stdin().read_line(&mut guess).expect("failed to read line");
 
-    println!("you guessed: {guess}");
+        let guess:u32 = match guess.trim().parse() {  // 数据类型转换 并进行数据校验
+            Ok(num) => num,
+            Err(_) => {
+                println!("输入的内容必须是数字");
+                continue;
+            }
+        };
 
-    let test = 666;
-    println!("the test value is {}", test);
+        println!("you guessed: {guess}");
+
+        match guess.cmp(&_sec_num) {
+            Ordering::Less => println!("too small"),
+            Ordering::Greater => println!("too big"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
