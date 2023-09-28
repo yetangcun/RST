a3089d8e0351ac49d9e5342502b66839bbd7efb4
yetangcun <302982814@qq.com>
2023-09-28 18:42:45 +0800

    ...

--- BaseDemo/Other/vars/src/main.rs
+++ BaseDemo/Other/vars/src/main.rs
@@ -1,7 +1,21 @@
 fn main() {
-    let mut varia = 5;
+    let varia1 = 1;   // 不可变变量
+    let mut varia = 5;  // 可变变量
     println!("you define a varia : {varia}");
 
     varia = 6;
     println!("you change the varia to : {varia}");
+
+    const CONSTVAL:u32 = 888888;  // 常量
+    println!("Hi: {CONSTVAL}");
+
+    let var0 = 111;
+    let var0 = var0 + 111;
+    println!("var0: {var0}");
+    {
+        let var0 = var0*3;
+        println!("var0:{var0}");
+    }
+
+    println!("last var0: {var0}");
 }
