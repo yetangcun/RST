fc72586443bb1e8d5ea79f78e1751dcba7b5f523
wuxj <wuxj@fujica.com.cn>
2023-10-04 16:54:08 +0800

    ...

--- BaseDemo/Other/vars/src/main.rs
+++ BaseDemo/Other/vars/src/main.rs
@@ -4,4 +4,18 @@ fn main() {
 
     varia = 6;
     println!("you change the varia to : {varia}");
+
+    let x = 6;
+    println!("current x val is : {}",x);
+
+    {
+        let x = x+1;
+        println!("current x value is : {}",x);
+    }
+
+    let x = x*2;
+    println!("now x val become: {}",x);
+
+    const XVAL:u32 = 3600*24*365;
+    println!("the const XVAL IS: {}",XVAL);
 }
