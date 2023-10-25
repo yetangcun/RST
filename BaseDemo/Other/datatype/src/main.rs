3e156aa6a8971dfe1a9fbd96c37db133e19c8f16
yetangcun <302982814@qq.com>
2023-10-25 10:36:23 +0800

    ..

--- BaseDemo/Other/datatype/src/main.rs
+++ BaseDemo/Other/datatype/src/main.rs
@@ -5,12 +5,12 @@ fn main() {
     let integer = 222u8;
     println!("the new integer val is {}", integer);
 
-    let floatval32:f64 = 16.8;
+    let floatval32:f32 = 16.8;
     println!("the f val is: {floatval32}");
 
-    let floatval = 16.88f64;
+    let floatval = 16.88f32;
     println!("the f32 val is: {}", floatval);
 
-    let result:f64 = floatval-floatval32;
+    let result:f32 = floatval-floatval32;
     println!("the result is: {}", result);
 }
