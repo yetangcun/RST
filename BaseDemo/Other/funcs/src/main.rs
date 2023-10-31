34ecfb317b1bcec2d0311ee754b0980cab283092
yetangcun <302982814@qq.com>
2023-10-31 15:53:46 +0800

    ...

--- BaseDemo/Other/funcs/src/main.rs
+++ BaseDemo/Other/funcs/src/main.rs
@@ -7,12 +7,11 @@ fn main() {
     println!("the result is {val}")
 }
 
-
 fn ano_func(params:i32) {
     println!("Here is a extension fn, and params is: {params}")
 }
 
 fn func_1() -> i32 {
     let const_v = 88888;
-    return const_v;
+    const_v
 }
