ab9937bd88e7d916ed7cc83920b9c93dedd75110
yetangcun <302982814@qq.com>
2023-11-02 18:35:48 +0800

    函数式改造

--- BaseDemo/Other/flowdemo/src/main.rs
+++ BaseDemo/Other/flowdemo/src/main.rs
@@ -1,4 +1,24 @@
 fn main() {
+    let temp = "xxx";
+    let condi = true;
+    let res = if condi {temp} else {"zzz"};
+    println!("the res is {res}");
+
+    // if条件控制
+    if_fn();
+
+    // loop循环
+    loop_fn();
+
+    // while循环
+    while_fn();
+
+    // for循环
+    for_fn();
+}
+
+
+fn if_fn() {
     let varial = 111;
 
     // if条件控制
@@ -9,13 +29,10 @@ fn main() {
     } else {
         println!("the result is not sure");
     }
+}
 
-    let temp = "xxx";
-    let condi = true;
-    let res = if condi {temp} else {"zzz"};
-    println!("the res is {res}");
 
-    // loop循环
+fn loop_fn() {
     let mut counts = 11; // mut修饰 可变变量
     let last_val = loop {
         if counts<1 {
@@ -25,17 +42,25 @@ fn main() {
         println!("Hi, good luck!");
     };
     println!("attentions, {last_val}!");
+}
 
-    // while循环
+
+
+fn while_fn() {
+    let mut counts = 0; // mut修饰 可变变量
     while counts<12 {
         println!("now, curr counts val is {counts}!");
         counts+=1;
     }
     println!("attentions, last counts val is {counts}!");
+}
 
-    // for循环
-    let count = counts + 1;
-    for num in (1..count).rev() {
+
+
+fn for_fn() {
+    let counts = 12; // mut修饰 可变变量
+    let count=counts + 1;
+    for num in 1..count {
         println!("again sure, curr num is {num}");
     }
     println!("well, all is over now!!!");
