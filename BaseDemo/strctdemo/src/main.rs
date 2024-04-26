f75d47ccee98852063aa44733628b4677d5253e3
yetangcun <302982814@qq.com>
2024-04-26 10:26:49 +0800

    ...

--- BaseDemo/strctdemo/src/main.rs
+++ BaseDemo/strctdemo/src/main.rs
@@ -1,3 +1,25 @@
 fn main() {
-    println!("Hello, world!");
+    let mut usr1 = User {
+        addr: String::from("yetangcun"),
+        name: String::from("xiaoxiao"),
+        age: 23
+    };
+
+    usr1.addr = String::from("yetangcun.");
+    println!("Hello, {0}", usr1.name);
+    println!("you're from {0}", usr1.addr);
+
+    let usr2 = User {
+        age: 24,
+        ..usr1
+    };
+    println!("you're from the {0}", usr2.addr);
+}
+
+
+// 结构体定义
+struct User {
+    name: String,
+    age:i32,
+    addr: String
 }
