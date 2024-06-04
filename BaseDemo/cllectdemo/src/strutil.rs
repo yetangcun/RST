de5d7382df2e775a4642e6eb7167bcf9a8552ddc
yetangcun <302982814@qq.com>
2024-06-04 18:59:35 +0800

    添加注释

--- BaseDemo/cllectdemo/src/strutil.rs
+++ BaseDemo/cllectdemo/src/strutil.rs
@@ -1,15 +1,16 @@
 pub fn str_fn() {
+    // rust字符串不支持索引取值
     let mut str = String::new();
     str.push_str("hello,");
 
-    let mut str0 = "欢迎您".to_string();
+    let mut str0 = "欢迎您".to_string();  // 等价于String::from("欢迎您")
     let str1 = String::from("my friends");
     let str2 = str + &str1;  // str被移动到str2中, str失效，str1继续有效可用
 
     println!("str0 is: {str0}, str1 is: {str1}, str2 is: {str2}");
 
-    str0.push_str(",最亲密的朋友");
-    str0.push('!');
+    str0.push_str(",最亲密的朋友"); // push_str方法追加字符串
+    str0.push('!'); // push方法是追加字符
     println!("Latest str0 is: {str0}");
 
     // format!宏 使用的都是字符串的引用,不获取所有权
@@ -21,9 +22,10 @@ pub fn str_fn() {
     let si = &s1[0..1]; println!("si:{si}");
 
     // 一个汉字占三个字节
-    let helo = "人间疾苦"; // Здравствуйте
-    let ss = &helo[0..3];
-    println!("ss:{ss}");
+    let helo = "Здравствуйте"; // 人间疾苦
+    let lens = helo.len();
+    let ss = &helo[0..4];
+    println!("ss:{ss}, {lens}");
 
     for hl in helo.chars() {
         println!("hl:{hl}");
@@ -33,8 +35,8 @@ pub fn str_fn() {
         println!("bty:{hl}");
     }
     
-    // find函数
-    let has = helo.find("间").unwrap();
+    // find函数 间
+    let has = helo.find("т").unwrap();
     println!("has:{has}");
 
     // replace函数
