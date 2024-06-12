a74e35491d0ecda7b924ae2b9a8daccb79b18c50
yetangcun <302982814@qq.com>
2024-06-12 19:03:57 +0800

    添加mysql操作方法

--- Common/DataExtensionLib/src/mysqlLib.rs
+++ Common/DataExtensionLib/src/mysqlLib.rs
@@ -9,14 +9,27 @@ fn get_conn() -> PooledConn {
     conn
 }
 
+pub fn do_insert() {
+    let mut conn = get_conn();
+    conn.exec_drop("insert into sys_user(Account,Passwd) values(?,?)", ("admin", "123456")).unwrap();
+}
+
+pub fn do_update() {
+    let mut conn = get_conn();
+    conn.exec_drop("update sys_user set Passwd=? where Account=?", ("123456", "admin")).unwrap();
+}
+
+pub fn do_del() {
+    let mut conn = get_conn();
+    conn.exec_drop("delete from sys_user where Account=?", ("admin",)).unwrap();
+}
+
 pub fn do_query() -> Vec<(i32,String,String)> {
     let mut conn = get_conn();
     let rs:Vec<(i32,String,String)> = 
     conn.query_map("select Id, Account, Passwd from sys_user", |(Id, Account, Passwd)|{(Id,Account,Passwd)}).unwrap();
-
     for (Id,Account,Passwd) in &rs {
         println!("ID: {}, Account: {}, Passwd:{}", Id, Account, Passwd);
     }
-
     rs
 }
