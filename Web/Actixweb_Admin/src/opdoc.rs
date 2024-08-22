38176c6b43edb8d8032d8f7a9d16331e301f3e51
yetangcun <302982814@qq.com>
2024-08-22 17:02:16 +0800

    .

--- Web/Actixweb_Admin/src/opdoc.rs
+++ Web/Actixweb_Admin/src/opdoc.rs
@@ -17,7 +17,7 @@ pub struct reqinput {
 }
 
 #[utoipa::path( // context_path = "/rsapi",
-    context_path = "/",
+    context_path = "/rsapi",
     responses(
         (status = 200, description = "succ", body = String),
         (status = 400, description = "fail")
@@ -27,12 +27,17 @@ pub struct reqinput {
 pub async fn lghdl(req_bdy: web::Json<lginput>) -> impl Responder {
     println!("{:?}", req_bdy.usr);
     let recv:lginput = req_bdy.into_inner();
-    let rt = String::from(format!("congratulations:{0}, you've logined success!",recv.usr));
-    HttpResponse::Ok().body(rt) 
+    let rt = String::from(format!("congratulations:{0}, you've logined success",recv.usr));
+    let obj = reqinput { 
+        usr: String::from(recv.usr), 
+        pwd: String::from(recv.pwd) 
+    };
+    web::Json(obj)
+    // HttpResponse::Ok().body(rt) 
 }
 
 #[utoipa::path(
-    context_path = "/",
+    context_path = "/rsapi",
     responses(
         (status = 200, description = "succ", body = String),
         (status = 400, description = "fail")
@@ -42,8 +47,13 @@ pub async fn lghdl(req_bdy: web::Json<lginput>) -> impl Responder {
 pub(super) async fn reqhdl(req_bdy: web::Json<reqinput>) -> impl Responder {
     println!("{:?}", req_bdy.usr);
     let recv:reqinput = req_bdy.into_inner();
-    let rt = String::from(format!("great:{0}, you've request over!",recv.usr));
-    HttpResponse::Ok().body(rt) 
+    let rt = format!("great:{0}, you've request over",recv.usr); //String::from();
+    let obj = reqinput { 
+        usr: String::from(recv.usr), 
+        pwd: String::from(recv.pwd) 
+    };
+    web::Json(rt)
+    // HttpResponse::Ok().body(rt) 
 }
 
 #[derive(OpenApi)]
