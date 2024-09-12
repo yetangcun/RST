0dd905eb1e9481584b040de272f8dc687748be8d
yetangcun <302982814@qq.com>
2024-09-12 19:38:13 +0800

    .

--- Common/CacheExtensionLib/src/rscaches/rdscache.rs
+++ Common/CacheExtensionLib/src/rscaches/rdscache.rs
@@ -1,21 +1,40 @@
-use redis::{Commands, Connection};
-use deadpool_redis::{Config as RedisConfig, Pool, Runtime};
+use redis::{Commands,Connection};
+use lazy_static::lazy_static;
+use std::sync::{Mutex, MutexGuard};
+use deadpool_redis::{Config as RedisConfig, Pool, Runtime,Connection as pool_conn};
 
 
 pub struct RdsPool {
     pool: Pool
 }
-
 impl RdsPool {
     pub fn new(url: &str, max_size:usize) -> Self {
-        let cfg = RedisConfig::from_url(url).unwrap();
+        let cfg = RedisConfig::from_url(String::from(url));
         let pool = cfg.create_pool(Some(Runtime::Tokio1)).unwrap();
         RdsPool {
             pool
         }
     }
 
-    pub fn get_conn(&self) -> Connection {
-        self.pool.get().unwrap()
+    pub async fn get_conn(&self) -> pool_conn {
+        self.pool.get().await.unwrap()
+    }
+}
+
+lazy_static! {
+    static ref RDS_CACHE: Mutex<Connection> = Mutex::new(RdsCache::new());
+}
+
+pub struct RdsCache {}
+
+impl RdsCache {
+    pub fn new() -> Connection {
+        let client = redis::Client::open("redis://192.168.30.166/").unwrap();
+        let mut con = client.get_connection().unwrap();
+        con
+    }
+
+    pub fn get_conn<'a>() -> MutexGuard<'a, Connection> {
+        RDS_CACHE.lock().unwrap()
     }
 }
