d643aaa6c586cca57d869da9c69223cba7bbb424
yetangcun <302982814@qq.com>
2024-09-18 11:39:34 +0800

    ?

--- Common/CacheExtensionLib/src/rscaches/rdscache.rs
+++ Common/CacheExtensionLib/src/rscaches/rdscache.rs
@@ -7,36 +7,40 @@ use deadpool_redis::{redis::AsyncCommands,Config as RedisConfig, Pool, Runtime,C
 // 复用连接对象--单例模式
 lazy_static! {
     static ref RDS_CACHE: Mutex<Connection> = Mutex::new(
-        // RdsCache::new("redis://:xiaoxiao@192.168.30.166:6379/2")
-        RdsCache::new("redis://127.0.0.1:6379/2")
+        RdsCache::bld_conn("redis://:xiaoxiao@192.168.30.166:6379/2")
+        // RdsCache::new("redis://127.0.0.1:6379/2")
     );
     static ref RDS_POOL_CACHE: Mutex<Pool> = Mutex::new(
-        // RdsPool::new("redis://:xiaoxiao@192.168.30.166:6379/2")
-        RdsPool::new("redis://127.0.0.1:6379/2")
+        RdsPool::get_pool("redis://:xiaoxiao@192.168.30.166:6379/2")
+        // RdsPool::new("redis://127.0.0.1:6379/2")
     );
 }
 
 // 缓存连接池
 pub struct RdsPool {}
 impl RdsPool {
-    pub fn new(url: &str) -> Pool {
+    pub fn tst_pool(&self) { // 方法，第一个参数总是self
+        println!("RdsPool::new()");
+    }
+
+    pub fn get_pool(url: &str) -> Pool { // 关联函数
         let cfg = RedisConfig::from_url(String::from(url));
         let pool = cfg.create_pool(Some(Runtime::Tokio1)).unwrap();
         pool
     }
 
-    pub async fn get_conn() -> PoolConnection {
+    pub async fn get_conn() -> PoolConnection { // 关联函数
         let pool = RDS_POOL_CACHE.lock().unwrap();
         pool.get().await.unwrap()
     }
 
-    pub async fn set_str(ky:&str, vl:String) -> bool{
+    pub async fn set_str(ky:&str, vl:String) -> bool{ // 关联函数
         let mut conn:PoolConnection = Self::get_conn().await;
         let res = conn.set::<&str, String, bool>(ky, vl).await.unwrap();
         res
     }
 
-    pub async fn get_str(key:&str) -> String {
+    pub async fn get_str(key:&str) -> String { // 关联函数
         let mut conn:PoolConnection = Self::get_conn().await;
         conn.get(key).await.unwrap()
     }
@@ -45,7 +49,11 @@ impl RdsPool {
 // 引用redis库
 pub struct RdsCache {}
 impl RdsCache {
-    pub fn new(url:&str) -> Connection {
+    pub fn tst_rds(&self) { // 方法，第一个参数总是self
+        println!("RdsCache::new()");
+    }
+    
+    pub fn bld_conn(url:&str) -> Connection {
         let client = redis::Client::open(url).unwrap();
         let mut con = client.get_connection().unwrap();
         con
